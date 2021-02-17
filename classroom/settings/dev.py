43d572f8ef17086dd0d634e8d7f666b9821dea62
Ankit Maurya <ankitmaurya18@cse.iiitp.ac.in>
2021-02-18 01:06:41 +0530

    Improved for dev. and prod. server

---
+++ classroom/settings/dev.py
@@ -0,0 +1,9 @@
+DEBUG = True
+from .base import REST_FRAMEWORK
+
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
+
+# REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES']= (
+#         'rest_framework.renderers.JSONRenderer',
+#     )
+
---
+++ classroom/settings/prod.py
@@ -0,0 +1,11 @@
+from .base import REST_FRAMEWORK
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES']= (
+        'rest_framework.renderers.JSONRenderer',
+    )
+
+
