13f41f87839b6304f356faeaf5dafa22cdb85505
Ankit Maurya <ankitmaurya18@cse.iiitp.ac.in>
2021-02-19 01:29:17 +0530

    removed code for api-router

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path,include
-from rest_framework import routers 
 from . import views
 
 
-router = routers.DefaultRouter()
-router.register("view", views.ClassroomViewSet)
 
 urlpatterns = [
     path("",views.UserList.as_view(),name="user-list"),
